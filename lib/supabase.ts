af3b6c36f2c0be1008d4b31a05c330909782f060
jxnobl <oabeledmarc15@gmail.com>
2026-09-25 01:57:47 +0800

    security: migrate to environment variables and allow .env.example

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,11 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =
-  process.env.NEXT_PUBLIC_SUPABASE_URL ||
-  "https://ndxtjumhcjsdczwykcjl.supabase.co";
-
-const supabaseAnonKey =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5keHRqdW1oY2pzZGN6d3lrY2psIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1OTE3NDEsImV4cCI6MjEwNDE2Nzc0MX0.ZJZGCaiEcDui3xHr0hcAthGdJEFQtFA39o4qlrtBiRE";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
